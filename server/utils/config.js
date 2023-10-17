b2ec9ae7a8374eca8c67e8895350fee5ac859553
martyanov-av <martyanov-av@yandex-team.ru>
2023-10-17 12:38:59 +0300

    fix: normalize saPrivateKey

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,7 +3,7 @@ import envconfig from "./envconfig.js";
 const ENV = {
     saId: envconfig.expectEnv('SA_ID')(),
     saAccessKey: envconfig.expectEnv('SA_ACCESS_ID')(),
-    saPrivateKey: envconfig.expectEnv('SA_PRIVATE_KEY')(),
+    saPrivateKey: envconfig.expectEnv('SA_PRIVATE_KEY')().replace(/\\n/g, '\n'),
     saSupervisorId: envconfig.expectEnv('STORAGE_SUPERVISOR_SA')('ajeqk7ra0h8ni9jascao'),
     saResourceId: envconfig.expectEnv('FOLDER_ID')('b1g1j115gl75k4sqiu0m')
 };
