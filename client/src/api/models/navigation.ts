ede808de9253c02a29806fa7c39ec8675ab8f512
martyanov-av <martyanov-av@yandex-team.ru>
2023-10-18 18:56:15 +0300

    feat: add cache fetch

--- client/src/api/models/navigation.ts
+++ client/src/api/models/navigation.ts
@@ -1,14 +1,12 @@
 import yaml from 'js-yaml';
-
+import fetchData, {Locale} from '../../utils/fetch-data';
 import {navigationEndpoint} from '~/configs/server';
 
 export async function Navigation() {
     try {
-        const res = await fetch(`${navigationEndpoint}/ru/published/navigation.yaml`);
-        if(res.ok){
-            return yaml.load(await res.text())
-        }
-        throw new Error(res.statusText);
+        const res = await fetchData('navigation', Locale.Ru, {navigationEndpoint});
+
+        return yaml.load(res.data)
     } catch (error) {
         throw error;
     }
