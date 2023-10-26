4919f29b085f53c1e85967f2bfd606907c9c9ffe
martyanov-av <martyanov-av@yandex-team.ru>
2023-10-26 16:50:24 +0300

    fix: wait till Deploy start

--- client/src/utils/common.ts
+++ client/src/utils/common.ts
@@ -8,3 +8,18 @@ export function getEnv() {
     return (process.env.APP_ENV as Env) || (process.env.DEV_MODE || false ? Env.Preprod : Env.Prod);
 }
 
+function pause(delay = 100) {
+    return new Promise(resolve => setTimeout(resolve, delay))
+}
+
+export async function retrier(operation, { attempts = Infinity, delay = 100 }) {
+    let result
+
+    for (let i = 0 ; i < attempts ; i++) {
+        result = await operation()
+        if (result) return result
+        await pause(delay)
+    }
+
+    return result
+}
