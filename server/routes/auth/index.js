3a527353223281fbd45d780f947d4530fc17d9cd
3y3 <3y3@ya.ru>
2024-07-22 19:08:07 +0600

    fix: Fix multipods state

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,6 +1,5 @@
 import {Router} from 'express';
-import { join } from 'node:path';
-import {randomBytes} from 'crypto';
+import {join} from 'node:path';
 import {Octokit} from 'octokit';
 import envConfig, {expectEnv} from '../../utils/envconfig.js';
 
@@ -19,27 +18,16 @@ const AUTH_SCOPES = ['public_repo'].join(',');
 
 export const router = () => {
     const router = new Router();
-    const githubState = new Map()
 
     router.get('/login/github', (req, res) => {
-        const state = randomBytes(20).toString('hex');
         const lang = req.get('Referrer')?.split('/')?.includes('en') ? 'en' : '';
-        githubState.set(state, lang)
 
-        res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${AUTH_SCOPES}&state=${state}`)
+        res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${AUTH_SCOPES}&state=${lang}`)
     });
 
     router.get('/login/github/callback', async (req, res) => {
         let accessToken;
         let user;
-        const state = req.query?.state;
-
-        if(!state || !githubState.has(state)) {
-            res.status(403);
-            res.send('Could not get github state back');
-            return;
-        }
-
         try {
             accessToken = await getAccessToken(req.query, req.fetch);
             user = await fetchGitHubUser(accessToken, req.fetch);
@@ -53,7 +41,7 @@ export const router = () => {
             req.session.accessToken = accessToken;
             req.session.githubId = user.id;
 
-            res.redirect(join('/', githubState.get(state), req.base || ''));
+            res.redirect(join('/', req.query?.state || '', req.base || ''));
         } else {
             res.status(403);
             res.send('Login did not succeed!')
