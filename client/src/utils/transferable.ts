e69e55e316bc24c670d3b3266e074e3c1de766c9
3y3 <3y3@ya.ru>
2024-07-30 21:53:21 +0300

    fix: Fix transferable data

--- client/src/utils/transferable.ts
+++ client/src/utils/transferable.ts
@@ -1,3 +1,18 @@
-export const parse = (string) => JSON.parse(decodeURIComponent(escape(atob(string))));
+// @see https://stackoverflow.com/a/52647441/1177115
+function b64e(string: string) {
+    return btoa(encodeURIComponent(string).replace(/%([0-9A-F]{2})/g,
+        function toSolidBytes(match, p1) {
+            return String.fromCharCode(Number('0x' + p1));
+        }));
+}
 
-export const stringify = (object) => btoa(unescape(encodeURIComponent(JSON.stringify(object))));
+// @see https://stackoverflow.com/a/52647441/1177115
+function b64d(string: string) {
+    return decodeURIComponent(atob(string).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
+export const parse = (string: string) => JSON.parse(b64d(string));
+
+export const stringify = (object: object) => b64e(JSON.stringify(object));
