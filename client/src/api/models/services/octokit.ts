8879ee6417928d72e9720a8327a33a63d2f528b9
martyanov-av <martyanov-av@yandex-team.ru>
2025-09-15 18:27:42 +0300

    fix: get accessToken before Octokit usage

--- client/src/api/models/services/octokit.ts
+++ client/src/api/models/services/octokit.ts
@@ -3,6 +3,10 @@ import {Octokit as InternalOctokit} from 'octokit';
 import { accessToken, fetch } from '~/configs/server';
 
 export function Octokit() {
+    if (!accessToken) {
+        throw new Error('GitHub access token is not available. Please authenticate first.');
+    }
+
     return new InternalOctokit({
         auth: accessToken,
         request: {
